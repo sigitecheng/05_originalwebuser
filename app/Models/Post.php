683e33fef92f3cad3fceb2219d1d2c59ec718945
Sigimatika <sigitechindo@gmail.com>
2023-03-29 08:28:12 +0700

    tambah data data yang ada

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,4 +11,10 @@ class Post extends Model
 
     // protected $fillable = ['title', 'slug', 'excerpt', 'body'];
     protected $guarded = ['id'];
+
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,6 +4,8 @@
 <article class="my-4">
     <h4><?= $post->title; ?></h4>
 
+    <p>By. Sigit Septiadi in {{ $post->category->nama_kategori }}</p>
+
     {!! $post->body !!}
 </article>
 <a class="btn btn-sn btn-success" href="/blog">
