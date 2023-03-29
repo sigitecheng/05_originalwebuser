b4cf5c92c2915ac1de252355d6c442b1fab95fa6
Sigimatika <sigitechindo@gmail.com>
2023-03-29 13:25:04 +0700

    tambah data baruu eee

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,7 +7,7 @@
 
 <article class="mb-5 border-bottom pb-5">
     <h3 class="text-decoration-none text-success">{{ $pos->title }}</h3>
-    <p>By. Sigit Iqlima in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->nama_kategori }}</a></p>
+    <p>By. Sigit Iqlima in <a href="/categories/{{ $pos->category->slug }}" class="text-decoration-none">{{ $pos->category->nama_kategori }}</a></p>
 
     <h5>{{ $pos->excerpt }}</h5>
 
