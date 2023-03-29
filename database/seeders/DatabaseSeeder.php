d10637642fd5bc888f932aff66f59503cba7c3aa
Sigimatika <sigitechindo@gmail.com>
2023-03-29 17:15:43 +0700

    tambah data yang diperlukan untuk post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -69,7 +69,7 @@ class DatabaseSeeder extends Seeder
             'slug' => 'judul-ketiga',
             'excerpt' => 'dsfgdfug dfgibndfigudf dfgindfgidnf dfgdhfug dfig dfgidfgi',
             'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus alias fuga, itaque mollitia exercitationem esse dolorem! Sed nemo ullam dignissimos praesentium, distinctio dolor nobis perferendis quo inventore omnis enim officia amet deleniti facilis sapiente quasi commodi repellat architecto at illum eos a ut non eveniet. Eos quasi beatae facere exercitationem voluptatibus est, ducimus corrupti illo quos tenetur iure explicabo amet obcaecati tempora laudantium enim vero odio animi? Iure aliquid velit at delectus fuga. Eius repudiandae, voluptas voluptatem odio cumque beatae! Illo quos unde earum sit? Expedita, nihil officiis? Fuga, corporis odit nobis doloremque debitis sit suscipit asperiores! Et, recusandae, dicta fuga eum exercitationem eveniet consectetur repellat asperiores sequi obcaecati repudiandae cumque quod maxime commodi, quaerat nostrum qui velit rerum dolores praesentium fugiat. Distinctio quo molestiae soluta, aspernatur eum cumque corrupti odio saepe debitis illum. Quasi voluptate amet in ab, numquam assumenda dolore maxime illo est, ullam id. Ratione, fugit! Explicabo.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima sed tempora ab dignissimos, vel tempore.</p>',
-            'category_id' => 3,
+            'category_id' => 2,
             'user_id' => 1
         ]);
     }
