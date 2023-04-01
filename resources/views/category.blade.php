852ae4f450221908049c703050ab205d16c90573
Sigimatika <sigitechindo@gmail.com>
2023-04-01 09:43:05 +0700

    tambah data baru untuk klass kategori

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,20 +1,23 @@
 @extends('layouts.main')
 @section('container')
-@foreach ($post as $pos)
 
+<h5><a href="/categories" class="text-decoration-none py-4 text-warning">Pilih Berdasarkan Kategori</a></h5>
+@foreach ($post as $pos)
 <h1 class="mb-5">Post Category : {{ $category }}</h1>
 
-<article class="my-5"></article>
-<h3>
-    <a href="/posts/<?= $pos->slug; ?>"><?= $pos->title; ?></a>
-</h3>
+<article class="my-5 border-bottom pb-5">
+    <h3>
+        <a href="/posts/<?= $pos->slug; ?>"><?= $pos->title; ?></a>
+    </h3>
 
-<h5><?= $pos->excerpt; ?></h5>
+    <h5><?= $pos->excerpt; ?></h5>
 
 
-<a class="btn btn-sn btn-success" href="/blog">
-    <= Back To Post</a>
+    <a class="btn btn-sn btn-danger mt-2" href="/blog">
+        <= Back To Post </a>
 
-        @endforeach
+</article>
 
-        @endsection
+@endforeach
+
+@endsection
